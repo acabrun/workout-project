76d7f0a1f5d89d6fa8b57dbc0abb70e226976c79
acabrun <acabrun@gmail.com>
2019-07-31 10:32:23 +1100

    proceed countdown function but none valid

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-/*chargement du son*/
+/*-------------chargement du son-----------------------*/
 
 let audio,
-exoTime,
-restTime,
-nbSerie,
-nbExo,
-currentExo,
-currentSerie,
-currentTime;
+    exoTime,
+    restTime,
+    nbSerie,
+    nbExo,
+    currentExo,
+    currentSerie,
+    currentTime,
+    timerID,
+    timerExoID;
 
 $('#introButton').on('click', function() {
     $('#intro').slideToggle();
@@ -20,11 +22,11 @@ $('#goButton').on('click', function() {
 })
 
 
-// initialization goal time
+// -------------------initialization goal time------------------------------
 function setGoalTime(goal, display) {
-    
+
     $(goal).on('blur', function(e) {
-        
+
         if (goal === '#setExoTime') {
             exoTime = e.target.value;
             $(display).text(exoTime);
@@ -35,11 +37,11 @@ function setGoalTime(goal, display) {
     })
 }
 
-// initialization goal exo & series
+// ------------------initialization goal exo & series-----------------------
 function setGoalExo(goal, display) {
-    
+
     $(goal).on('blur', function(e) {
-        
+
         if (goal === '#setNbSeries') {
             nbSerie = e.target.value;
             currentSerie = 1;
@@ -59,7 +61,7 @@ setGoalExo('#setNbSeries', '#nbSerie');
 setGoalExo('#setNbOfExo', '#nbExo');
 
 
-// display current time
+// ----------------------------display current time-------------------------------
 function displayCurrentTime() {
     let date = new Date();
     let h = date.getHours();
@@ -67,56 +69,127 @@ function displayCurrentTime() {
     currentTime = h + 'h' + m;
     $('#currentTime').text(currentTime);
 }
+
 displayCurrentTime();
 setInterval(displayCurrentTime, 15000);
 
-// timer
+// --------------------------------timer--------------------------------------------
+let sTimer = 0,
+    mTimer = 0;
+
 function timer() {
-    let s = 0,
-        m = 0;
-    setInterval(function() {
-        s += 1;
-        if (s<60) {
-            $('#timer').text(m + "'" + s + '"')
-        }else if(s===60) {
-            s=0;
-            m += 1;
-            $('#timer').text(m + "'" + s + '"');
+
+    timerID = setInterval(function() {
+        sTimer += 1;
+        if (sTimer < 60) {
+            $('#timer').text(mTimer + "'" + sTimer + '"')
+        } else if (sTimer === 60) {
+            sTimer = 0;
+            mTimer += 1;
+            $('#timer').text(mTimer + "'" + sTimer + '"');
         }
 
     }, 1000)
 }
 
-// countdown    
-function countdown(typeOfTimer) {
-    let timeArray = typeOfTimer.split(':');
-    let m = timeArray[0];
-    let s = timeArray[1];
-    console.log(m +' et '+s);
+// -------------------------COUNTDOWN ----------A REFAIRE TOTALEMENT-------------------------
+let timeArray, mCountdown, sCountdown; 
 
-    setInterval(function() {
-        s -= 1;
-        if (s>0) {
-            $('#exoTime').text(m + "'" + s + '"')
-        }else if (s===0) {
-            s = 59;
-            m -= 1;
-            $('#exoTime').text(m + "'" + s + '"')
+function countdown(typeOfTimer, yet) {  // yet param is used to keep countdown after call startStopButton
+                                        // without initialization
+    
+    if (currentSerie === nbSerie && currentExo === nbExo) {
+        //END OF WORKOUT
+        console.log('End of workout');
+
+    } else {
+        switch (yet) {
+        case false:  // INITIALIZATION
+            timeArray = typeOfTimer.split(':');
+            mCountdown = timeArray[0];
+            sCountdown = timeArray[1];
+
+            timerExoID = setInterval(function() {
+                
+                if (sCountdown > 0 && mCountdown>=0) {
+                    sCountdown -= 1;
+                    $('#exoTime').text(mCountdown + "'" + sCountdown + '"')
+                } else if (sCountdown === 0 && mCountdown>0) {
+                    sCountdown = 59;
+                    mCountdown -= 1;
+                    $('#exoTime').text(mCountdown + "'" + sCountdown + '"')
+                }else if (sCountdown === 0 && mCountdown === 0) {
+                    console.log(typeOfTimer)
+                    if (typeOfTimer === exoTime) {
+                        countdown(restTime, false)
+                    } else if (typeOfTimer === restTime) {
+                        if (currentExo === nbExo) {
+                            currentSerie += 1;
+                            currentExo = 1;
+                            $('#setNbOfExo').text(currentExo + '/' + nbExo);
+                            $('#setNbOfSeries').text(currentSerie + '/' + nbSerie);
+                            countdown(exoTime, false);
+                        } else {
+                            currentExo += 1;
+                            $('#setNbOfExo').text(currentExo + '/' + nbExo);
+                            countdown(exoTime, false);
+                        }
+                    }
+                    
+                    
+                } else{ console.log('error') }
+
+            }, 1000);
+            break;
+
+        case true:  // NONE INITIALIZATION
+            timerExoID = setInterval(function() {
+
+                
+                if (sCountdown > 0 && mCountdown>=0) {
+                    sCountdown -= 1;
+                    $('#exoTime').text(mCountdown + "'" + sCountdown + '"')
+                } else if (sCountdown === 0 && mCountdown>0) {
+                    sCountdown = 59;
+                    mCountdown -= 1;
+                    $('#exoTime').text(mCountdown + "'" + sCountdown + '"')
+                } else if (sCountdown === 0 && mCountdown === 0) {
+                    console.log('rest 3')
+                    countdown(restTime, false)
+                    
+                } else {
+                    console.log('else if error : secondes = ' + sCountdown + 'minutes = ' + mCountdown);
+                }
+
+            }, 1000);
+            break;
+
+        default:
+            return console.log('error');
         }
 
-    }, 1000)
+    }
 }
 
- 
-
-
-// start/stop button
+// ---------------------------start/stop button----------------------------------------
+let nbClick = 0;
 $('#startStopButton').on('click', function() {
-    timer();
-    countdown(exoTime);
+    nbClick += 1;
+
+    if (nbClick % 2 === 0) {
+        $('#startStopButton').text('START');
+        $('#startStopButton').css("background-color", "#54EE47");
+        clearInterval(timerID);
+        clearInterval(timerExoID);
+    } else {
+        $('#startStopButton').text('STOP');
+        $('#startStopButton').css("background-color", "red");
+        timer();
+        if (nbClick === 1) {
+            countdown(exoTime, false);
+        } else {
+            countdown(exoTime, true);
+        }
+        
+    }
 })
-
-
-
-
-
